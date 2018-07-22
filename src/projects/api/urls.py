c6b216577d6e6421507f93b4c406d29fdc82d72b
ELI <2037157400@qq.com>
2018-07-22 15:32:25 +0800

    Update urls.py

--- src/projects/api/urls.py
+++ src/projects/api/urls.py
@@ -17,9 +17,9 @@ from .views import (
 urlpatterns = [
     url(r'^$', ProjectAPIView.as_view(), name='list'),
     url(r'^(?P<id>\d+)/$', ProjectAPIDetailView.as_view(), name='detail'),
-    url(r'^(?P<id>\d+)/target$', ProjectTargetDetailView.as_view(), name='target'),
+    url(r'^(?P<id>\d+)/target/$', ProjectTargetDetailView.as_view(), name='target'),
     url(r'^(?P<id>\d+)/release/$', ProjectReleaseView.as_view(), name='release'),
-    url(r'^(?P<id>\d+)/result', ProjectResultView.as_view(), name='result'),
+    url(r'^(?P<id>\d+)/result/$', ProjectResultView.as_view(), name='result'),
     url(r'^(?P<id>\d+)/contributors/$', ContributorsListView.as_view(), name='contributors'),
     url(r'^(?P<id>\d+)/edit_contributors/$', ProjectEditContributorsView.as_view(), name='edit-contributors'),
     url(r'^verify/$', ProjectVerifyListView.as_view(), name='verify-projects-list'),
