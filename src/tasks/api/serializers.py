9071daa102f8c5165284445adfb1c603fc67078d
lily <973548751@qq.com>
2018-08-09 14:50:26 +0800

    修改Task Detail API的字段

--- src/tasks/api/serializers.py
+++ src/tasks/api/serializers.py
@@ -5,6 +5,11 @@ from tasks.models import Task
 
 class TaskSerializer(serializers.ModelSerializer):
     # text_file_path = serializers.SerializerMethodField(read_only=True)
+    project_type = serializers.SerializerMethodField(read_only=True)
+    target = serializers.SerializerMethodField(read_only=True)
+    target_name = serializers.SerializerMethodField(read_only=True)
+    target_entity = serializers.SerializerMethodField(read_only=True)
+    target_description = serializers.SerializerMethodField(read_only=True)
     text_content = serializers.SerializerMethodField(read_only=True)
     contributor_name = serializers.SerializerMethodField(read_only=True)
 
@@ -13,6 +18,11 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'project',
+            'project_type',
+            'target',
+            'target_name',
+            'target_entity',
+            'target_description',
             'text_content',
             'label',
             'contributor',
@@ -23,6 +33,21 @@ class TaskSerializer(serializers.ModelSerializer):
 
     # def get_file_path(self, obj):
     #     return obj.text_file.url
+
+    def get_project_type(self, obj):
+        return obj.project.project_type
+
+    def get_target(self, obj):
+        return obj.project.project_target.id
+
+    def get_target_name(self, obj):
+        return obj.project.project_target.name
+
+    def get_target_entity(self, obj):
+        return obj.project.project_target.entity
+
+    def get_target_description(self, obj):
+        return obj.project.project_target.description
 
     def get_text_content(self, obj):
         path = obj.file_path
