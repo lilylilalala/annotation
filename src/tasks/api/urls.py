5f777e523806c7d28e55939df4ee94e9d530fd07
lily <973548751@qq.com>
2018-08-15 10:54:23 +0800

    增加Task Update API，用户可修改答案

--- src/tasks/api/urls.py
+++ src/tasks/api/urls.py
@@ -5,5 +5,5 @@ from .views import TaskDetailView, TaskUpdateView
 
 urlpatterns = [
     url(r'^(?P<id>\d+)/$', TaskDetailView.as_view(), name='detail'),
-    url(r'^update/(?P<id>\d+)$', TaskUpdateView.as_view(), name='update'),
+    url(r'^update/(?P<id>\d+)/$', TaskUpdateView.as_view(), name='update'),
 ]
